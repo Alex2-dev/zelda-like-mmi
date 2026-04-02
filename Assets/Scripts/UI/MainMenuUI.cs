4ff3ebca3b35297e88d6aaee67f795d37154a076
thedeadyt <sofyalexisleclubdes5@gmail.com>
2026-04-02 11:26:34 +0200

    feat: add MainMenuUI controller (play/settings/quit panels)

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+/// <summary>
+/// Controller du menu principal.
+/// 3 panels : MainPanel (boutons principaux), SlotPanel (sélection save), SettingsPanel.
+/// </summary>
+public class MainMenuUI : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject m_mainPanel;
+    public GameObject m_slotPanel;
+    public GameObject m_settingsPanel;
+
+    void Start()
+    {
+        ShowMain();
+    }
+
+    public void OnPlayClicked()
+    {
+        m_mainPanel.SetActive(false);
+        m_slotPanel.SetActive(true);
+    }
+
+    public void OnSettingsClicked()
+    {
+        m_mainPanel.SetActive(false);
+        m_settingsPanel.SetActive(true);
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    public void ShowMain()
+    {
+        m_mainPanel.SetActive(true);
+        m_slotPanel.SetActive(false);
+        m_settingsPanel.SetActive(false);
+    }
+}
